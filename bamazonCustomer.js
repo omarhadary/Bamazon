a2c490da1a86fbbdde2b0a02ce2152f38b33cd66
Omar Elhadary <Omar Elhadary>
2016-12-21 03:07:50 -0800

    validation for user input complete. check for sufficient stock, update of stock and total purchase cost working.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,47 +1,62 @@
-var inquirer = require ("inquirer");
-var mysql = require ("mysql");
-
+var inquirer = require("inquirer");
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user:"root",
-    password: "hadary83",
-    database: "Bamazon"
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "hadary83",
+	database: "Bamazon"
 });
-
 connection.connect(function(err) {
-    if(err) throw err;
-    console.log("connected as id "+connection.threadId);
+	if (err) throw err;
+	console.log("connected as id " + connection.threadId);
 });
-
 var itemsForSale = function() {
-    connection.query("SELECT * FROM products", function (err, res) {
-        if(err) throw err;
-        console.log(JSON.stringify(res, null, 2));
-        setTimeout(buyPromprt, 2000);
-    });
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		console.log(JSON.stringify(res, null, 2));
+		setTimeout(buyPromprt, 2000);
+	});
 };
 var buyPromprt = function() {
-    inquirer.prompt([{
-        name: "product",
-        type: "input",
-        message: "What is the ID of the product you would like to buy?",
-        validate: function (value) {
-            var valid = !isNaN(parseFloat(value));
-            return valid || "Please enter a number between 1 and 10";
-        },
-        filter: Number
-    } , {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to buy?",
-        validate: function (value) {
-            var valid = !isNaN(parseFloat(value));
-            return valid || "Please enter a number";
-        },
-        filter: Number
-    }]).then(function(answer) {
-        console.log("answers");
-    });
+	inquirer.prompt([{
+		name: "product",
+		type: "input",
+		message: "What is the ID of the product you would like to buy?",
+		validate: function(value) {
+			var valid = parseFloat(value) < 11 && parseFloat(value) > 0;
+			return valid || "Please enter a number between 1 and 10";
+		},
+		filter: Number
+	}, {
+		name: "quantity",
+		type: "input",
+		message: "How many would you like to buy?",
+		validate: function(value) {
+			var valid = !isNaN(parseFloat(value));
+			return valid || "Please enter a number";
+		},
+		filter: Number
+	}]).then(function(answer) {
+		connection.query("SELECT * FROM products WHERE ?", {
+			item_id: answer.product
+		}, function(err, res) {
+			var stockCount = res[0].stock_quantity;
+			var purchaseCost = res[0].price * answer.quantity;
+			if (res[0].stock_quantity < parseInt(answer.quantity)) {
+				console.log("Insufficient quantity, only " + stockCount + " left!");
+				setTimeout(itemsForSale, 2000);
+			} else {
+				connection.query("UPDATE products SET ? WHERE ?", [{
+					stock_quantity: stockCount - answer.quantity
+				}, {
+					item_id: answer.product
+				}], function(err, res) {
+					console.log("Total cost of your purchase is $" + purchaseCost);
+					setTimeout(itemsForSale, 3000);
+				});
+			}
+		})
+	});
 };
 itemsForSale();
