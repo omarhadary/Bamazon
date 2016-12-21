8e5c8f2d65fddb89955e9d4e3ad9d6c7c194ee64
Omar Elhadary <Omar Elhadary>
2016-12-21 01:33:09 -0800

    promrpts for user input to buy working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user:"root",
-    password: "hadary83"
+    password: "hadary83",
     database: "Bamazon"
 });
 
@@ -14,10 +14,34 @@ connection.connect(function(err) {
     console.log("connected as id "+connection.threadId);
 });
 
-var appStart = function() {
-    inquirer.prompt({
-
-    }).then(function(answer) {
-
+var itemsForSale = function() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if(err) throw err;
+        console.log(JSON.stringify(res, null, 2));
+        setTimeout(buyPromprt, 2000);
     });
 };
+var buyPromprt = function() {
+    inquirer.prompt([{
+        name: "product",
+        type: "input",
+        message: "What is the ID of the product you would like to buy?",
+        validate: function (value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || "Please enter a number between 1 and 10";
+        },
+        filter: Number
+    } , {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to buy?",
+        validate: function (value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || "Please enter a number";
+        },
+        filter: Number
+    }]).then(function(answer) {
+        console.log("answers");
+    });
+};
+itemsForSale();
