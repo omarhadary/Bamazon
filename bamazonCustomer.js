a795cae61596f27171d5973c10700b042697011b
Omar Elhadary <Omar Elhadary>
2016-12-21 23:32:56 -0800

    created a startApp function to allow user to exit the app. added comments to highlight pseudocode. added node version to package.json and created composer.json to deploy to heroku.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,18 +7,38 @@ var connection = mysql.createConnection({
 	password: "hadary83",
 	database: "Bamazon"
 });
+//connect to database
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId);
 });
+//function to display the 10 different products
 var itemsForSale = function() {
 	connection.query("SELECT * FROM products", function(err, res) {
 		if (err) throw err;
 		console.log(JSON.stringify(res, null, 2));
-		setTimeout(buyPromprt, 2000);
+		//prompt for user to select which item to buy is 
+        // displayed 2 seconds after the products are listed
+		setTimeout(startApp, 2000);
 	});
 };
-var buyPromprt = function() {
+//prompt user to decide if they want to make a purchase or exit the app
+var startApp = function() {
+	inquirer.prompt([{
+		name: "start",
+		type: "list",
+		message: "Are you interested in making a purchase?",
+		choices: ["Yes, I want to make a purchase.", "No thanks!"]
+	}]).then(function(answer) {
+		if (answer.start === "No thanks!") {
+			return console.log("Hope to see you again soon.");
+		} else {
+			buyPrompt();
+		}
+	});
+};
+//function to prompt user to select which product they want to buy and quantity
+var buyPrompt = function() {
 	inquirer.prompt([{
 		name: "product",
 		type: "input",
@@ -38,15 +58,26 @@ var buyPromprt = function() {
 		},
 		filter: Number
 	}]).then(function(answer) {
+		//check database using product ID entered by user against item_id column
 		connection.query("SELECT * FROM products WHERE ?", {
 			item_id: answer.product
 		}, function(err, res) {
+			//store the stock quantity left for the user selected product 
+            // in order to update the stock count after the purchase
 			var stockCount = res[0].stock_quantity;
+			//calculates the total purchase cost of the product times 
+            // the quantity to dispaly to user after they make the purchase
 			var purchaseCost = res[0].price * answer.quantity;
+			//checks if stock quantity is less than quantity user woud like to purchase, 
+            // displays insufficient quantity message and runs the user prompt function 
+            // to select another product or a different quantity
 			if (res[0].stock_quantity < parseInt(answer.quantity)) {
 				console.log("Insufficient quantity, only " + stockCount + " left!");
-				setTimeout(itemsForSale, 2000);
+				setTimeout(startApp, 2000);
 			} else {
+				//if there is enough quantity, then reduce stock quantity by order quantity, 
+                // display the total purchase cost and runs the app from the start by displaying 
+                // the 10 products with the updated stock quantity
 				connection.query("UPDATE products SET ? WHERE ?", [{
 					stock_quantity: stockCount - answer.quantity
 				}, {
